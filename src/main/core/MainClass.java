70451466b9587c0502bc8485c372f9c747cf176b
xhs872383661 <xhs1990hunan@163.com>
2016-04-20 21:12:03 +0800

    第一次ok运行，Log4j.xml生效，不同级别的信息，输出到不同的日志文件中

--- src/main/core/MainClass.java
+++ src/main/core/MainClass.java
@@ -1,9 +1,25 @@
 package core;
 
+import org.apache.log4j.Logger;
+
 /**
  * Created by xhs on 2016/4/20.
  * @author  xionghs
  */
 public class MainClass
 {
+    private static Logger logger = Logger.getLogger(MainClass.class);
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        // 记录debug级别的信息
+        logger.debug("This is debug message.");
+        // 记录info级别的信息
+        logger.info("This is info message.");
+        // 记录error级别的信息
+        logger.error("This is error message.");
+    }
 }
